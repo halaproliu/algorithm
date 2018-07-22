e18d3e96fa9a297e9adf7da4bc07bb9c42ccb577
luffy <luffybryant@gmail.com>
2018-07-22 22:36:37 +0800

    init

---
+++ src/algorithm.js
@@ -0,0 +1,57 @@
+// 快速排序
+function quickSort(arr) {
+    if (arr.length <= 1) {
+        return arr;
+    }
+    var pivotIndex = Math.floor(arr.length / 2);
+    var pivot = arr.splice(pivotIndex, 1)[0];
+    var left = [];
+    var right = [];
+    for (var i = 0; i < arr.length; i++) {
+        if (arr[i] <= pivot) {
+            left.push(arr[i]);
+        } else {
+            right.push(arr[i]);
+        }
+    }
+    return quickSort(left).concat([pivot], quickSort(right));
+}
+
+// 冒泡排序
+function bubbleSort(arr) {
+    for (var i = 0; i < arr.length - 1; i++) {
+        for (var j = 0; j < arr.length - 1 - i; j++) {
+            if (arr[j] > arr[j + 1]) {
+                var temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+    return arr;
+}
+
+// 斐波那契数列
+function fabonacci (num) {
+    if (num === 1) return 0
+    if (num === 2) return 1
+    return fabonacci(num - 1) + fabonacci(num - 2)
+}
+
+// 阶乘
+function factorial (num) {
+    if (num === 1) return num
+    return num * factorial(num - 1)
+}
+
+// 将数字转成货币形式
+function toThousands(num) {
+    var result = [], counter = 0;
+    num = (num || 0).toString().split('');
+    for (var i = num.length - 1; i >= 0; i--) {
+        counter++;
+        result.unshift(num[i]);
+        if (!(counter % 3) && i != 0) { result.unshift(','); }
+    }
+    return result.join('');
+}
